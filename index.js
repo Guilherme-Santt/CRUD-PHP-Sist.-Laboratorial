bc3c107fe8143213bb2ebe82c474c1f11fa82c65
Guilherme-Santt <guisant@icloud.com>
2024-01-15 17:39:51 -0300

    função onclick javascript->menu de opções

---
+++ index.js
@@ -0,0 +1,15 @@
+function lmenu(){
+    let div =  document.getElementById("lh");
+    div.style.display="inline"
+    div.style.transition="0.3s"
+    }
+  
+  function fmenu(){
+    let div =  document.getElementById("lh");
+    div.style.display="none";
+  }
+  
+  function tbanner(){
+    let div =  document.getElementById("banner");
+    div.style.backgroundColor="black";
+  }
