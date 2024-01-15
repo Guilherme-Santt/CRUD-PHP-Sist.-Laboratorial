1200c25d75ec1abc0444d66be320ef70486aac50
Guilherme-Santt <guisant@icloud.com>
2024-01-15 17:53:28 -0300

    Tabela de clientes cadastrados no sistema na index

--- index.php
+++ index.php
@@ -11,6 +11,23 @@
     $query = $mysqli->query($sqlcode);
     $cliente = $query->fetch_assoc();
 
+    function formatar_data($data){
+        return implode('/', array_reverse(explode('-', $data)));
+    };
+    // FORMATAR TELEFONE PARA VISUALIZAÇÃO COM CARACTERES
+    function formatar_telefone($telefone){
+        $ddd = substr ($telefone, 0, 2);
+        $parte1 = substr ($telefone, 2, 5);
+        $parte2 = substr ($telefone, 7);
+        return "($ddd) $parte1-$parte2";
+    }
+    include('conexao.php');
+    // COMANDO SQL PARA CONSULTAR CLIENTES
+    $sql_clientes   = "SELECT * FROM clientes";
+    // COMANDO QUERY, PARA EXECUTAR COMANDO SQL
+    $query_clientes = $mysqli->query($sql_clientes) or die($mysqli->error);
+    // COMANDO NUM ROWS, PARA CONTAR QUANTIDADE DADOS NO BANCO
+    $num_clientes   = $query_clientes->num_rows;
 ?>
 
 <!DOCTYPE html>
@@ -33,7 +50,7 @@
         <img onclick="lmenu()" class="h-img" src="imagens/hamburger.png">
         <div id="lh" class="h-menu"><br>
             <a onclick="fmenu()">X</a>
-            <b>Ofertas imperdíveis da black! ↓</b>
+            <a>Menu de configuções</a>
             <br><br>
             <a>Menu1</a><hr><br>
             <a href="logout.php">Encerrar sessão</a>
@@ -52,54 +69,60 @@
         <p>Olá, <b><?php echo $cliente['nome']?></b></p>
         <p></p>
     </header>
-
-
-    <div class="nav-g">
-        <div class="nav-item-title">
-            <h1>Lançamentos - Masculinos</h1>
-        </div>
-
-
-    
-        <div class="nav-email">
-            <h1>Receba novidades no e-mail</h1><hr>
-            <b>Cadastre-se e seja um dos primeiros a saber todas as novidades.</b><br><br>            </b>
-            <input type="text" id="search-bar-email" placeholder="Digite seu email">
-            <button>Cadastre-se</button><br><br>
-            <img src="imagens/o-email.png">
-
-        </div>
-
-        <div class="nav-rodape">
-            <div class="nav-social">
-                <h1>Nossa redes sociais</h1>
-                <img src="imagens/instagram.png">
-                <img src="imagens/facebook.png">
-                <img src="imagens/tiktok.png">
-                <img src="imagens/youtube.png">
-                <img src="imagens/whatsapp.png"><br>
-                <b>@SantImports</b><br><br>
-                <b>Rua Santa Genoveva, 217 - CEP: 06700505
-                </b>
-            </div>
-            <div class="nav-social">
-                <h1>Categorias</h1>
-                <a src="#">Masculino</a><br>
-                <!-- <a src="#">Feminino</a><br>
-                <a src="#">Infantil</a><br>
-                <a src="#">Acessórios</a><br>
-                <a src="#">Buckets</a><br>
-                <a src="#">Lançamentos</a><br> -->
-            </div>
-            <div class="nav-social">
-                <h1>Contatos</h1>
-                <a src="#">Trabalhe conosco</a><br>
-                <!-- <a src="#">Suporte</a><br>
-                <a src="#">Loja 1 - Endereço</a><br>
-                <a src="#">Loja 1 - Endereço</a><br>
-                <a src="#">Loja 1 - Endereço</a><br><hr>
-                <b>santimports@mail.com.br</b> -->
-            </div>
+    <div>
+        <div class="nav-g">
+            <div class="nav-clientes">
+                <a href="http://127.0.0.1/projetoCadastro/cadastrar_cliente.php">Cadastrar cliente</a>
+                <h1>Listagem de clientes</h1>
+                <p>Esses são os clientes cadastrados no seu sistema</p>
+                <table border="1" cellpadding="10">
+                    <thead>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>Telefone</th>
+                        <th>Nascimento</th>
+                        <th>Data de cadastro</th>
+                        <th>Ações</th>
+                    </thead>
+                    <tbody> 
+                        <?php 
+                            if($num_clientes == 0) { 
+                        ?> 
+                                <tr>
+                                    <td colspan="7">Nenhum cliente foi cadastrado</td>
+                                </tr>
+                        <?php }
+                            else{ 
+                                while($cliente = $query_clientes->fetch_assoc()){
+                                $telefone ="Não informado!";
+                                if(!empty($cliente['telefone'])){
+                                    $telefone = formatar_telefone($cliente['telefone']);   
+                                }
+                                $nascimento = "Nascimento não informada!";
+                                if(!empty($cliente['nascimento'])){
+                                    $nascimento = formatar_data($cliente['nascimento']);
+                                }
+                                $data_cadastro = date("d/m/y H:i:s", strtotime($cliente['data']));
+                        ?>     
+                                <tr>
+                                    <td><?php echo $cliente['id']?>     </td>
+                                    <td><?php echo $cliente['nome']?>   </td>
+                                    <td><?php echo $cliente['email']?>  </td>
+                                    <td><?php echo $telefone; ?>  </td>
+                                    <td><?php echo $nascimento ?>   </td>
+                                    <td><?php echo $data_cadastro;?>    </td>
+                                    <td>
+                                        <a class="edit" href="editar_cliente.php?id=<?php echo $cliente['id']?>">Editar</a>
+                                        <a class="error" href="deletar_cliente.php?id=<?php echo $cliente['id']?>">Deletar</a>
+                                    </td>
+                                </tr>             
+                        <?php
+                            }
+                        }
+                        ?>
+                    </tbody>
+                </table>
         </div>
     </div>
     <script src="index.js"></script>
