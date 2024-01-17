2af5ce0228fea10cb62957b9e6196a658bb00944
Guilherme-Santt <guisant@icloud.com>
2024-01-17 16:33:12 -0300

    Ajuste HTML & código tela de pacientes

--- Arquivos PHP/pacientes.php
+++ Arquivos PHP/pacientes.php
@@ -30,72 +30,76 @@
     }
             }
         
-            // VERIFICAÇÃO DA INSERÇÃO DOS CAMPOS POST NO FORM
-            $error = "";
-            if(count($_POST) > 0){
-                $email = $_POST['email'];
-                $sexo = $_POST['sexo'];
-                $nome = $_POST['nome'];
-                $endereco = $_POST['endereco'];
-                $telefone = $_POST['telefone'];
-                $nascimento = $_POST['nascimento'];
+    // VERIFICAÇÃO DA INSERÇÃO DOS CAMPOS POST NO FORM
+    $error = "";
+    if(count($_POST) > 0){
+        $email = $_POST['email'];
+        $sexo = $_POST['sexo'];
+        $nome = $_POST['nome'];
+        $endereco = $_POST['endereco'];
+        $telefone = $_POST['telefone'];
+        $nascimento = $_POST['nascimento'];
         
-                if(empty($_POST['email']) || !filter_var($email, FILTER_VALIDATE_EMAIL)){
-                $error = "Campo e-mail obrigatório*";
-                }
-                if(empty($_POST['endereco'])){
-                    $error = "Campo endereço obrigatório*";
-                }
-                if(!empty($_POST['sexo']) ){
-                    if(strlen($sexo) != 3){
-                        $error = "Escreva campo FEM ou MAS";
-                    }
-                }    
-                if(empty($_POST['nome']) || Strlen($nome) < 3 || Strlen($nome) > 100){
-                    $error = "Campo nome obrigatório*";
-                }
+        if(empty($_POST['email']) || !filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $error = "Campo e-mail obrigatório*";
+            }
+
+        if(empty($_POST['endereco'])){
+            $error = "Campo endereço obrigatório*";
+            }
+
+        if(!empty($_POST['sexo']) ){
+            if(strlen($sexo) != 3){
+                $error = "Escreva campo FEM ou MAS";
+            }
+            }    
+
+        if(empty($_POST['nome']) || Strlen($nome) < 3 || Strlen($nome) > 100){
+            $error = "Campo nome obrigatório*";
+            }
         
-                if(!empty($nascimento)){
-                    $pedacos = explode('/', $nascimento);
-                    if(count($pedacos) == 3){
+        if(!empty($nascimento)){
+            $pedacos = explode('/', $nascimento);
+                if(count($pedacos) == 3){
                         $nascimento = implode ('-', array_reverse($pedacos)); 
-                    }
-                    else{
+                }
+                else{
                         $erro = "A data de nascimento deve ser preenchido no padrão dia/mes/ano";
-                    }
-                }    
-                if(!empty($telefone)){
-                    $telefone = limpar_texto($telefone);
-                    if(strlen($telefone) != 11){
-                        $erro = "O telefone deve ser preenchido no padrão (11) 98888-8888";
-                    }
                 }
-                if($error){
-        
-                }else{
-                    $sql_codeverify = "SELECT * FROM pacientes WHERE email = '$email'";
-                    $query_c = $mysqli->query($sql_codeverify);
-                    $paciente = $query_c->fetch_assoc();
-                    $verify = $query_c->num_rows;
-                        if($verify){
-                            $error = "paciente já cadastrado!";
-                        }
-                        else{
-                            $sqlinsert = "INSERT INTO pacientes (nome, email, endereco, telefone, nascimento, data, sexo)  
-                            values ('$nome', '$email', '$endereco', '$telefone', '$nascimento', NOW(), '$sexo')";
-                            $queryinsert = $mysqli->query($sqlinsert);
-                                if($queryinsert){
-                                    $sucess = "Cadastrado com sucesso";
-                                }
-                        }
+            }    
+
+        if(!empty($telefone)){
+            $telefone = limpar_texto($telefone);
+                if(strlen($telefone) != 11){
+                    $erro = "O telefone deve ser preenchido no padrão (11) 98888-8888";
                 }
+            }
+        if($error){
         
-            }   
-            // VISUALIZAÇÃO INFORMAÕES USUARIO CAMPO NO HEADER
-            $id = $_SESSION['usuario'];
-            $sqlcode = "SELECT * FROM clientes WHERE id = '$id'";
-            $query = $mysqli->query($sqlcode);
-            $cliente = $query->fetch_assoc();
+        }else{
+            $sql_codeverify = "SELECT * FROM pacientes WHERE email = '$email'";
+            $query_c = $mysqli->query($sql_codeverify);
+            $paciente = $query_c->fetch_assoc();
+            $verify = $query_c->num_rows;
+                if($verify){
+                    $error = "paciente já cadastrado!";
+                    }
+                else{
+                    $sqlinsert = "INSERT INTO pacientes (nome, email, endereco, telefone, nascimento, data, sexo)  
+                        values ('$nome', '$email', '$endereco', '$telefone', '$nascimento', NOW(), '$sexo')";
+                    $queryinsert = $mysqli->query($sqlinsert);
+                        if($queryinsert){
+                            $sucess = "Cadastrado com sucesso";
+                            }
+                    }
+            }
+        
+        }   
+    // VISUALIZAÇÃO INFORMAÕES USUARIO CAMPO NO HEADER
+    $id = $_SESSION['usuario'];
+    $sqlcode = "SELECT * FROM clientes WHERE id = '$id'";
+    $query = $mysqli->query($sqlcode);
+    $cliente = $query->fetch_assoc();
 ?>
 
 
@@ -106,6 +110,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<link rel="stylesheet" href="../Arquivos CSS/usuarios.css">
 <body>         
     <a href="index.php">Retornar pagina inicial</a> 
     <a href="cadastro_pacientes.php">Cadastrar Pacientes</a>      
@@ -191,10 +196,10 @@
 
             <button type="submit" name="cadastrar">Cadastrar</button>
         </form>
-        <?php 
-            if(isset($sucess)){echo'<p class="sucess">'. $sucess . '</p>' ;}
-            if($error){echo '<p class="error">'. $error . '</p>' ;}   
-        ?>
+<?php 
+    if(isset($sucess)){echo'<p class="sucess">'. $sucess . '</p>' ;}
+    if($error){echo '<p class="error">'. $error . '</p>' ;}   
+?>
     </div>    
     <script src="../Arquivos JS/index.js"></script>
 </body>
