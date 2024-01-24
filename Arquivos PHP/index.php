58b92bdd19998f57a7ba3bee90741f080711ced4
Guilherme-Santt <guisant@icloud.com>
2024-01-23 21:01:49 -0300

    Comentando

--- Arquivos PHP/index.php
+++ Arquivos PHP/index.php
@@ -1,18 +1,24 @@
 <?php
+include('conexao.php');
 if(!isset($_SESSION)){
     session_start();
     if(!isset($_SESSION['usuario'])){
         die('Você não está logado!' . '<a href="login.php">Clique aqui para logar</a>');
     }    
 }
-include('conexao.php');
+
 $id = $_SESSION['usuario'];
 
-// QUANTIDADE E INFORMAÇÕES D EUSUARIOS
+// INFORMAÇÕES DE USUARIOS
 $sqlcode = "SELECT * FROM clientes WHERE id = '$id'";
 $query = $mysqli->query($sqlcode);
-$cont_user = $query->num_rows;
 $usuario = $query->fetch_assoc();
+
+// QUANTIDADE DE USUARIOS
+$sqlcode = "SELECT * FROM clientes WHERE id";
+$query = $mysqli->query($sqlcode);
+$usuario = $query->fetch_assoc();
+$cont_user = $query->num_rows; 
 
 // QUANTIDADE E INFORMAÇÕES DE PACIENTES
 $sqlcode = "SELECT * FROM pacientes";
