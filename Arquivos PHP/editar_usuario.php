b72b9ee497489263547de3caa5c47f6a7fa14691
Guilherme-Santt <guisant@icloud.com>
2024-01-30 17:18:39 -0300

    Formatação de pastas

--- Arquivos PHP/editar_usuario.php
+++ Arquivos PHP/editar_usuario.php
@@ -28,48 +28,34 @@ $id = intval($_GET['id']);
 include('conexao.php');
 
 $error = "";
-// CAMPOS POST DO INSERT APÓS DAR O INPUT
 if(count($_POST) > 0){
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $unidade = $_POST['unidade'];
     $telefone = $_POST['telefone'];
     $nascimento = $_POST['nascimento'];
-// VERICAÇÃO SE O INPUT NOME ESTÁ VAZIO OU ENTRE 3 Á 100 DÍGITOS 
-    if(empty($nome) || Strlen($nome) < 3 || Strlen($nome) > 100){
+    if(empty($nome) || Strlen($nome) < 3 || Strlen($nome) > 100)
         $error = "Por favor, Prencha o campo nome corretamente. Capacidade mínima 3 dígitos! ";
-    }
-// VERIFICAÇÃO SE O INPUT E-MAIL ESTÁ VAZIO OU NO PADRÃO DE VALIDADE EMAIL
-    if(empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)){
+    if(empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL))
         $error = "Por favor, Prencha o campo e-mail corretamente.";
-    }   
-// VERIFICAÇÃO SE O INPUT NASCIMENTO ESTÁ OU DIFERENTE DE 10 DÍGITOS
     if(empty($nascimento) || strlen($nascimento) != 10){
         $error = "Campo nascimento deve ser preenchido no padrão dia/mês/ano";
-    }
-    // SE NÃO TIVER VAZIO, VAI UTILIZAR FUNÇÃO PADRÃO PHP PARA EXPLODIR AONDE ESTIVER / 
-        else{
-            $pedacos = explode('/', $nascimento);
-    // VERIFICANDO SE APÓS EXPLODIR ESTÁ EM 3 PARTES EX: DIA MÊS ANO, PARA ENTÃO JUNTAR E REVERTER, SEGUINDO PADRÃO EUA
-            if(count($pedacos) == 3){
-                $nascimento = implode ('-', array_reverse($pedacos)); 
-            }
+    }else{
+        $pedacos = explode('/', $nascimento);
+        if(count($pedacos) == 3){
+            $nascimento = implode ('-', array_reverse($pedacos)); 
+        }
     }    
-    // VERIFICANDO SE INPUT TELEFONE ESTÁ VAZIO
     if(empty($telefone)){
         $error ="Campo telefone obrigatório";
+    }else{
+        $telefone = limpar_texto($telefone);
+        if(strlen($telefone) != 11){
+            $error = "O telefone deve ser preenchido no padrão (11) 98888-8888";
+        }
     }
-    // CASO NÃO TIVER, VAI TIRAR AS CARACTERES DO INPUT UTILIZANDO A FUNÇÃO RETIRADA DO SITE ZEROBUGS.COM. SE FICAR DIFERENTE DE 11 DÍGITOS[11995910318] -> VAI APARECER MENSAGEM DE ERRO 
-        else{
-            $telefone = limpar_texto($telefone);
-            if(strlen($telefone) != 11){
-                $error = "O telefone deve ser preenchido no padrão (11) 98888-8888";
-            }
-    }
-// VERIFICANDO SE TEM ALGUM ERRO
     if($error){
     }
-    // CASO NÃO TIVER NENHUM ERRO NAS VERIFICAÇÕES DE ERRO DOS CAMPOS, VAI ATUALIZAR AS INFORMAÇÕES DO USUÁRIO. CASO NÃO TIVER ALTERADO, SERÁ ATUALIZADO DO MESMO JEITO COM AS MESMAS INFORMAÇÕES INSERIDAS ATRÁVES DO SELECT FROM A BAIXO.
     else{
         $sql_code = "UPDATE clientes
         SET nome   = '$nome', 
@@ -77,14 +63,13 @@ if(count($_POST) > 0){
         unidade    = '$unidade',
         telefone   = '$telefone',
         nascimento = '$nascimento' WHERE id   = '$id'";
-        
         $deu_certo = $mysqli->query($sql_code);
             if($deu_certo){
                 $sucess ="Atualizado com sucesso";
                 unset($_POST);
             }     
     }         
-    }
+}
 // }
 // SELECT FROM NA TABELA CLIENTES, PARA PUXAR INFORMAÇÕES DOS PACIENTES PARA OS CAMPOS INPUT
 include('conexao.php');
